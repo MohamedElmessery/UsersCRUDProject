e1fbb358eecc9c54b29c4a42cf72cf6cfeb61fc3
Mohamed Nasser <mohamed.elmessery6@gmail.com>
2021-01-19 02:07:40 +0200

    Ui

---
+++ CRUDProject/Controllers/UsersController.cs
@@ -0,0 +1,53 @@
+﻿using CRUDProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CRUDProject.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Users
+        public ActionResult Index()
+        {
+            IEnumerable<UserData> listuser;
+            HttpResponseMessage response = Service.WebApiClient.GetAsync("Users").Result;
+            listuser = response.Content.ReadAsAsync<IEnumerable<UserData>>().Result;
+            return View(listuser);
+        }
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            if (id == 0)
+                return View(new UserData());
+            else
+            {
+                HttpResponseMessage response = Service.WebApiClient.GetAsync("Users/" + id.ToString()).Result;
+                return View(response.Content.ReadAsAsync<UserData>().Result);
+            }
+        }
+        [HttpPost]
+        public ActionResult AddOrEdit(UserData user)
+        {
+            if (user.User_Id == 0)
+            {
+                HttpResponseMessage response = Service.WebApiClient.PostAsJsonAsync("Users", user).Result;
+                TempData["SuccessMessage"] = "Saved Successfully";
+            }
+            else
+            {
+                HttpResponseMessage response = Service.WebApiClient.PutAsJsonAsync("Users/" + user.User_Id,user).Result;
+                TempData["SuccessMessage"] = "Updated Successfully";
+            }
+            return RedirectToAction("Index");
+        }
+        public ActionResult Delete(int id)
+        {
+            HttpResponseMessage response = Service.WebApiClient.DeleteAsync("Users/" + id.ToString()).Result;
+            TempData["SuccessMessage"] = "Deleted Successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ CRUDProject/Models/UserData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace CRUDProject.Models
+{
+    //Form Api
+    public class UserData
+    {
+        public int User_Id { get; set; }
+        [Display(Name ="Name"),Required(ErrorMessage ="User Name is Required")]
+        public string User_Name { get; set; }
+        [Display(Name = "Age"),Required(ErrorMessage = "Age is Required"),Range(14,30)]
+        public Nullable<int> User_Age { get; set; }
+        [Display(Name = "Adress"),Required(ErrorMessage ="User Name is Required")]
+        public string User_Adress { get; set; }
+    }
+}
